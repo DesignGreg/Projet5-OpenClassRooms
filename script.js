14fea9320073128877d789b1f80b0bd4e53683c4
Gregory <huyghegregory@yahoo.fr>
2018-11-19 11:43:01 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,38 @@
+/* Générateur de citations pour personnages de jeux de rôles, univers science-fiction ou fantastique. */
+
+
+/* Générateur View */
+/*-----------------*/
+
+
+
+/* Générateur Controller */
+/*-----------------------*/
+
+// Génération de la phrase SF
+
+
+/* Générateur Model */
+/*------------------*/
+
+// Tableau 1 - SF
+const firstSFArray = ["Je suis un", "Je serais un", "J'ai été un"];
+const firstSFArrayF = ["Je suis une", "Je serais une", "J'ai été une"];
+ 
+// Tableau 2 - SF
+const secondSFArray = ["un pilote", "un aventurier", "un explorateur", "un agent", "un chercheur", "un colon"];
+const secondSFArrayF = ["une pilote", "une aventurière", "une exploratrice", "une agent", "une chercheuse", "un colon"];
+
+// Tableau 3 - SF
+const thirdSFArray = ["de Vénus", "de Mars", "d'Alpha Centauri", "des tréfonds de la Galaxie", "du Néant"];
+
+// Tableau 1 - Fantasy
+const firstFanArray = ["Je suis un", "Je serais un", "J'ai été un"];
+const firstFanArrayF = ["Je suis une", "Je serais une", "J'ai été une"];
+
+// Tableau 2 - Fantasy
+const secondFanArray = ["un marchand", "un guerrier", "un barbare", "un sorcier", "un élémentaire", "un voleur"];
+const secondFanArrayF = ["une marchande", "une guerrière", "une barbare", "une sorcière", "un élémentaire", "une voleuse"];
+
+// Tableau 3 - Fantasy
+const thirdFanArray = ["d'Hyrule", "de Cimmérie", "des Terres du Milieu", "de Poudlard", "du Pandémonium", "de l'Autre Monde"];
