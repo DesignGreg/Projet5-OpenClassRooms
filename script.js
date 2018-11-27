4e5813de0023ca3d12feb916b75d02d2e8b21e4b
Gregory <huyghegregory@yahoo.fr>
2018-11-27 15:12:44 +0100

    correction indentation

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // Tableau 1 - SF
 const firstSFArray = ["Je suis", "Je serais", "J'ai été"];
 const firstSFArrayF = ["Je suis", "Je serais", "J'ai été"];
- 
+
 // Tableau 2 - SF
 const secondSFArray = ["un pilote", "un aventurier", "un explorateur", "un agent", "un chercheur", "un colon"];
 const secondSFArrayF = ["une pilote", "une aventurière", "une exploratrice", "une agent", "une chercheuse", "un colon"];
@@ -31,16 +31,16 @@ const thirdFanArray = ["d'Hyrule", "de Cimmérie", "des Terres du Milieu", "de P
 
 // Init Event Handler pour les Boutons
 function init() {
-    document.getElementById('B1').onclick=function() {   
+    document.getElementById('B1').onclick = function () {
         sciFi();
     };
-    document.getElementById('B2').onclick=function() {   
+    document.getElementById('B2').onclick = function () {
         sciFiF();
     };
-    document.getElementById('B3').onclick=function() {   
+    document.getElementById('B3').onclick = function () {
         fantasy();
     };
-    document.getElementById('B4').onclick=function() {   
+    document.getElementById('B4').onclick = function () {
         fantasyF();
     };
 }
@@ -54,80 +54,80 @@ let value = 1;
 function change() {
     const liste = document.getElementById('select');
     const choix = liste.options[liste.selectedIndex].value;
-    
+
     value = choix;
 }
 
 
-    
-    // Génération de la phrase SF
-    // Masculin
+
+// Génération de la phrase SF
+// Masculin
 
 function sciFi() {
-    
+
     if (value > 1) {
-        for(let x = 1; x <= value; x++) {
+        for (let x = 1; x <= value; x++) {
             generator(firstSFArray, secondSFArray, thirdSFArray);
             displayQuote();
         }
     } else {
-            generator(firstSFArray, secondSFArray, thirdSFArray);
-            displayQuote();
+        generator(firstSFArray, secondSFArray, thirdSFArray);
+        displayQuote();
     }
 }
 
 // Féminin
 function sciFiF() {
-    
+
     if (value > 1) {
-        for(let x = 1; x <= value; x++) {
+        for (let x = 1; x <= value; x++) {
             generator(firstSFArrayF, secondSFArrayF, thirdSFArray);
             displayQuote();
         }
     } else {
-            generator(firstSFArrayF, secondSFArrayF, thirdSFArray);
-            displayQuote();
-    }  
+        generator(firstSFArrayF, secondSFArrayF, thirdSFArray);
+        displayQuote();
+    }
 }
 
 // Génération de la phrase fantastique
 // Masculin
 function fantasy() {
-    
+
     if (value > 1) {
-        for(let x = 1; x <= value; x++) {
+        for (let x = 1; x <= value; x++) {
             generator(firstFanArray, secondFanArray, thirdFanArray);
             displayQuote();
         }
     } else {
-            generator(firstFanArray, secondFanArray, thirdFanArray);
-            displayQuote();
-    } 
+        generator(firstFanArray, secondFanArray, thirdFanArray);
+        displayQuote();
+    }
 }
 
 // Féminin
 function fantasyF() {
-    
+
     if (value > 1) {
-        for(let x = 1; x <= value; x++) {
+        for (let x = 1; x <= value; x++) {
             generator(firstFanArrayF, secondFanArrayF, thirdFanArray);
             displayQuote();
         }
     } else {
-            generator(firstFanArrayF, secondFanArrayF, thirdFanArray);
-            displayQuote();
-    } 
+        generator(firstFanArrayF, secondFanArrayF, thirdFanArray);
+        displayQuote();
+    }
 }
 
 
 // Génération d'une phrase - Fonction
 let result = "";
 
-function generator (array1, array2, array3) {
+function generator(array1, array2, array3) {
     const random1 = array1[Math.floor(Math.random() * array1.length)];
     const random2 = array2[Math.floor(Math.random() * array2.length)];
     const random3 = array3[Math.floor(Math.random() * array3.length)];
-    
+
     result = random1 + " " + random2 + " " + random3;
 }
 
@@ -138,17 +138,16 @@ function generator (array1, array2, array3) {
 
 // Affichage des résultats
 function displayQuote() {
-    
+
     const quote = result;
-    
+
     const tagReponse = document.createElement("p");
     const reponse = document.createTextNode(quote);
     tagReponse.appendChild(reponse);
-    
+
     const resultats = document.getElementById("resultats");
     resultats.appendChild(tagReponse);
-    
+
     // Console
     console.log(quote);
-    
 }
